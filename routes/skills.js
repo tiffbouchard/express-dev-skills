d038543991a1616be041909871718fe9b25f7d3c
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-06-07 15:37:23 -0400

    Add new controllers

--- routes/skills.js
+++ routes/skills.js
@@ -4,5 +4,8 @@ const skillsCtrl = require('../controllers/skills');
 
 router.get('/', skillsCtrl.index);
 router.get('/:id', skillsCtrl.show);
+router.get('/new', skillsCtrl.new);
+router.post('/', skillsCtrl.create);
+router.delete('/:id', skillsCtrl.delete);
 
 module.exports = router;
