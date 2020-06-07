55b58e27eb42e480f0af2c71f64b7d7da308204e
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-06-07 12:51:50 -0400

    Add index and show controllers

---
+++ controllers/skills.js
@@ -0,0 +1,20 @@
+const Skill = require('../models/skill');
+
+module.exports = {
+  index,
+  show,
+};
+
+function index(req, res) {
+  res.render('skills/index', {
+    skills: Skill.getAll()
+  });
+}
+
+function show(req, res) {
+  const skill = Skills.getOne(req.params.id);
+  res.render('skills/show', {
+    skill
+  });
+}
+
